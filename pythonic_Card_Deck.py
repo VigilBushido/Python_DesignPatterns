d6b9de986374d50949f5b9ed1b4ac07c0a665277
VigilBushido <vigilbushido@gmail.com>
2021-11-20 19:39:36 -0500

    added pythonic CardDeck.py

---
+++ pythonic_Card_Deck.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 20 19:24:25 2021
+
+@author: vigilbushido
+"""
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class SpanishDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+    
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                       for rank in self.ranks]
+        
+    def __len__(self):
+        return len(self.cards)
+    
+    def __getitem__(self, position):
+        return self._cards[position]
+    
+def main():
+    beer_card = Card('7', 'diamonds')
+    print(beer_card)
+    
+if __name__ == "__main__":
+    main()
+    
